a2e739ae42b3b5a162fc6831d3d2a6b4ea1de3fb
amakzy <ahmadamakxy128@gmail.com>
2023-11-23 04:34:22 +0200

    Add files via upload

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,8 +8,15 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
+        if cls:
+            cls_obj = {}
+            for key, val in FileStorage.__objects.items():
+                if val.__class__.__name__ is cls.__name__:
+                    cls_obj[key] = val
+            return cls_obj
+
         return FileStorage.__objects
 
     def new(self, obj):
@@ -23,7 +30,7 @@ class FileStorage:
             temp.update(FileStorage.__objects)
             for key, val in temp.items():
                 temp[key] = val.to_dict()
-            json.dump(temp, f)
+            json.dump(temp, f, indent=4)
 
     def reload(self):
         """Loads storage dictionary from file"""
@@ -45,6 +52,14 @@ class FileStorage:
             with open(FileStorage.__file_path, 'r') as f:
                 temp = json.load(f)
                 for key, val in temp.items():
-                        self.all()[key] = classes[val['__class__']](**val)
-        except FileNotFoundError:
+                    self.all()[key] = classes[val['__class__']](**val)
+        except Exception:
+            pass
+
+    def delete(self, obj=None):
+        """Delete obj"""
+        try:
+            obj_id = str(obj.__class__.__name__ + "." + str(obj.id))
+            del FileStorage.__objects[obj_id]
+        except Exception:
             pass
